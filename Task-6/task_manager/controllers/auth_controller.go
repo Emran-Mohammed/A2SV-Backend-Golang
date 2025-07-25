7005540bd700740f9a4f69278605d506547e4f4f
Emran Mohammed <emran.seid@aastustudent.edu.et>
2025-07-25 17:30:34 +0300

    just to avoid the yello err

--- Task-6/task_manager/controllers/auth_controller.go
+++ Task-6/task_manager/controllers/auth_controller.go
@@ -1,57 +1,57 @@
 package controllers
 
 import (
-	"context"
 	"net/http"
 	"task_manager/data"
 	"task_manager/models"
-	"time"
-
 	"github.com/gin-gonic/gin"
+	"time"
+	"context"
 )
 
 type authHandler struct {
-	userManager *data.UserManager
+	userManager * data.UserManager
 }
 
-func NewUserHandler(um *data.UserManager) *authHandler {
+func NewUserHandler(um *data.UserManager)*authHandler{
 	return &authHandler{userManager: um}
 }
 
-func (ah *authHandler) Register(c *gin.Context) {
+
+func (ah *authHandler) Register(c *gin.Context ){
 	ctx, cancel := context.WithTimeout(c.Request.Context(), 10*time.Second)
 	defer cancel()
 
 	var user models.User
-	if err := c.ShouldBindJSON(&user); err != nil {
+	if err:= c.ShouldBindJSON(&user); err != nil{
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
 	}
 	err := ah.userManager.Register(ctx, user)
-	if err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
-		return
+	if err != nil{
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message":err.Error()})
+		return 
 	}
 
 	c.IndentedJSON(http.StatusCreated, gin.H{"message": "User registered successfully"})
 }
 
-func (ah *authHandler) Login(c *gin.Context) {
+func (ah *authHandler) Login(c *gin.Context){
 	ctx, cancel := context.WithTimeout(c.Request.Context(), 10*time.Second)
 	defer cancel()
 
 	var user models.User
-	if err := c.ShouldBindJSON(&user); err != nil {
+	if err:= c.ShouldBindJSON(&user); err != nil{
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
 	}
 
 	jwtToken, err := ah.userManager.Login(ctx, user)
-	if err != nil {
-		c.IndentedJSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+	if err != nil{
+		c.IndentedJSON(http.StatusUnauthorized, gin.H{"message":err.Error()})
 		return
 	}
 
-	c.IndentedJSON(http.StatusOK, gin.H{"message": "user logged in successfully", "token": *jwtToken})
+	c.IndentedJSON(http.StatusOK, gin.H{"message":"user logged in successfully","token":*jwtToken})
 
 }
